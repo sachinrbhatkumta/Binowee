ecc22d8393da51d8fad5a2ae9f9d819c17940023
Sachin <sachinrbhatkumta@gmail.com>
2013-07-20 18:02:56 +1000

    Second commit

---
+++ Binowee/src/controller/HelloController.java
@@ -0,0 +1,13 @@
+package controller;
+
+public class HelloController {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("I am inside hello controller");
+
+	}
+
+}
