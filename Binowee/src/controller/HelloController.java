a095b9f5a402b614e0e77d3d482620dd0fb2e0b0
User <User@User-PC>
2013-07-20 19:06:06 +1000

    Commit from opc

--- Binowee/src/controller/HelloController.java
+++ Binowee/src/controller/HelloController.java
@@ -9,6 +9,7 @@ public class HelloController {
 		System.out.println("I am inside hello controller");
 		System.out.println("Hello World2");
 		System.out.println("Hello World3");
+		System.out.println("Hello World4 from office");
 
 	}
 
